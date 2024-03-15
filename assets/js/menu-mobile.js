545353864d808322bf3c3b3f07f6a400c152cde3
LuisFerRodVar <luisfrv1@estudiantec.cr>
2024-03-15 11:34:41 -0600

    first commit

---
+++ assets/js/menu-mobile.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    let mobile_btn = document.querySelector('.navbar__mobile-btn');
+    let mobile_menu = document.querySelector(".menu-mobile");
+    mobile_btn.addEventListener("click", () => {
+        let show = document.querySelector(".menu-mobile--show");
+        if (show) {
+
+            mobile_menu.classList.remove("menu-mobile--show");
+        } else {
+            mobile_menu.classList.add("menu-mobile--show");
+        }
+    })
+    window.addEventListener('resize', () => {
+        let window_width = parseInt(document.body.clientWidth);
+        if (window_width > 1000) {
+            mobile_menu.classList.remove("menu-mobile--show");
+        }
+    })
+    let btn_close = document.querySelector(".menu-mobile__close");
+    btn_close.addEventListener("click", () => {
+        mobile_menu.classList.remove("menu-mobile--show");
+    })
+    let menu_item = document.querySelectorAll(".menu-mobile__item");
+    menu_item.forEach(item => {
+        item.addEventListener("click", (event) => {
+            let submenu = item.lastElementChild;
+            if (submenu.className == "menu-mobile__submenu-mobile") {
+                if (submenu.style.display === 'block') {
+                    submenu.style.display = 'none';
+                    submenu.style.visibility = 'hidden';
+                    submenu.style.opacity = '0';
+                    
+                }else{
+                    submenu.style.display = 'block';
+                    submenu.style.visibility = 'visible';
+                    submenu.style.opacity = '1';
+
+                    
+                }
+
+            }
+        })
+    })
+});
